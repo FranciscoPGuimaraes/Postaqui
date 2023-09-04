e841f8b01ef81c58c5aa65717bacb59651d84821
Francisco Pereira Guimarães <franciscop.guimaraes04@gmail.com>
2023-09-03 21:54:44 -0300

    Formulario de remetente

---
+++ src/pages/components/Information.jsx
@@ -0,0 +1,16 @@
+import './Information.css'
+
+function Information({name, data, link}) {
+    return (
+        <a href={link}>
+            <div className="info">
+                <h3>{name}</h3>
+                <p>{data.address.street} - {data.address.number}</p>
+                <p>{data.address.neighborhood} - {data.address.city}</p>
+                <p>{data.address.cep}</p>
+            </div>
+        </a>
+    );
+}
+
+export default Information
